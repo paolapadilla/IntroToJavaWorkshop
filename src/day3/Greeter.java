51af398643d6630f86543fdd9805f0b944a12699
paolapadilla <league@WorthyMini.local>
2017-03-29 12:01:28 -0700

    i like the class

---
+++ src/day3/Greeter.java
@@ -0,0 +1,20 @@
+package day3;
+
+import javax.swing.JOptionPane;
+
+import jdk.nashorn.internal.scripts.JO;
+
+public class Greeter {
+public static void main(String[] args) {
+	System.out.println("early bird gets the worm");
+	JOptionPane.showMessageDialog(null, "hello my name is Rocko");
+	JOptionPane.showMessageDialog(null, "you are awesome");
+	JOptionPane.showMessageDialog(null, "you are pretty");
+	JOptionPane.showMessageDialog(null, "I like your hair");
+	JOptionPane.showMessageDialog(null, "I like your glasses");
+	JOptionPane.showMessageDialog(null, "I like your eyes thnak you");
+	
+	
+	
+}
+}
---
+++ src/day3/Snooper.java
@@ -0,0 +1,20 @@
+package day3;
+
+import javax.swing.JOptionPane;
+
+public class Snooper {
+public static void main(String[] args) {
+	 String name=JOptionPane.showInputDialog("what is your name");
+	 JOptionPane.showMessageDialog(null, "hello "+ name);
+	 String phone=JOptionPane.showInputDialog(null, "give me your phone number "+name);
+	 String house=JOptionPane.showInputDialog(null,"where do you live");
+	
+	
+	
+	
+	
+}
+	
+	
+	
+}
